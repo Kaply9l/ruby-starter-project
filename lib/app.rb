d980e263ae316c430c3382e33ef585ce3186d713
Kaply9l <kislov.igor@list.ru>
2022-01-14 19:22:03 +0700

    Remove nils

--- lib/app.rb
+++ lib/app.rb
@@ -2,24 +2,20 @@ require_relative 'temperature_builder'
 
 @types = %w[C K F]
 @states = %w[y n]
-@exit = 'y'
-until @exit == 'n'
-  @builder = TemperatureBuilder.new
+loop do
+  builder = TemperatureBuilder.new
   puts 'Input type temperature from convert (C, K, F):'
-  @from = $stdin.gets.chomp until @types.include?(@from)
+  from = $stdin.gets.chomp until @types.include?(from)
   puts 'Input value:'
-  @value = $stdin.gets.to_f
+  value = $stdin.gets.to_f
   puts 'Input type temperature to convert (C, K, F):'
-  @to = $stdin.gets.chomp until @types.include?(@to) && (@to != @from)
-  @result = @builder.type_set(@from)
-                    .value_set(@value)
-                    .convert_to(@to)
+  to = $stdin.gets.chomp until @types.include?(to) && (to != from)
+  result = builder.type_set(from)
+                  .value_set(value)
+                  .convert_to(to)
 
-  puts "Convert from #{@from} to #{@to} result: #{@result}"
+  puts "Convert from #{from} to #{to} result: #{result}"
   puts 'Wanna repeat? [y, n]'
-  @exit = $stdin.gets.chomp
-  @exit = $stdin.gets.chomp until @states.include?(@exit)
-  @from = nil
-  @value = nil
-  @to = nil
+  exit = $stdin.gets.chomp until @states.include?(exit)
+  break if exit == 'n'
 end
